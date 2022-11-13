392da89ddbf4f9326b4a82966acbaae989b4dc99
Dwayne Dyer <iStagnate@gmail.com>
2022-11-12 20:44:58 -0500

    added zircon i guess

--- src/main/java/net/stag/mobularmechanisms/item/ModItems.java
+++ src/main/java/net/stag/mobularmechanisms/item/ModItems.java
@@ -12,10 +12,13 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, MobularMechanisms.MOD_ID);
 
-    //ores
-    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
+    //ores and ingots
+    public static final RegistryObject<Item> MOBIUS_INGOT = ITEMS.register("mobius_ingot",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MOBULAR_MECHANISMS)));
     public static final RegistryObject<Item> FINGER_OF_DEATH = ITEMS.register("finger_of_death",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MOBULAR_MECHANISMS)));
+
+    public static final RegistryObject<Item> RAW_MOBIUS = ITEMS.register("raw_mobius",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MOBULAR_MECHANISMS)));
     //gadgets
     //other
